9ac7ca4ae74f65d9229fbdfb64cadc7b1d0c282e
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-20 19:48:49 -0300

    criando o menu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace PomodoroNoConsole
 {
@@ -7,10 +8,31 @@ namespace PomodoroNoConsole
 
   static void Main(string[] args)
   {
-
+    Console.WriteLine("=============================");
+        Console.WriteLine(" _____   ____  __  __  ____   ");
+        Console.WriteLine("|  __ \\ / __ \\|  \\/  ||  __ \\ ");
+        Console.WriteLine("| |__) | |  | | \\  / || |  | |");
+        Console.WriteLine("|  ___/| |  | | |\\/| || |  | |");
+        Console.WriteLine("| |    | |__| | |  | || |__| |");
+        Console.WriteLine("|_|     \\____/|_|  |_||_____/");
+        Console.WriteLine("=============================");
+        Console.WriteLine("ESCOLHA UMA DAS OPÇÕES ABAIXO: ");
+        Console.WriteLine("=============================");
+        Console.WriteLine("||  1 - POMODORO(25 MINUTOS) || ");
+        Console.WriteLine("||  2 - PAUSA(5 MINUTOS)     || ");
+        Console.WriteLine("||  3 - SAIR DO PROGRAMA     || ");
+        Console.WriteLine("=============================");
+        int chosenNumber = int.Parse(Console.ReadLine());
   }
 
-  
+  static void Start(int chosenNumber){
+    switch(chosenNumber){
+        case "1" : 
+    }
+    
+
+
+  }
     }
 
 }
